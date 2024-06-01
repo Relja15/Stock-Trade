05dfa8014f89641afd1c2e3dbbc9127a0a38a5df
Relja15 <reljasusic1234@gmail.com>
2024-06-01 12:00:53 +0200

    init commit

---
+++ src/main/java/com/viser/StockTrade/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.viser.StockTrade.repository;
+
+import com.viser.StockTrade.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
